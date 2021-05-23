473c53023975c180b4427a83171906980d3fc80a
Madi <72963376+Madi-S@users.noreply.github.com>
2021-05-23 16:21:25 +0600

    dark and light mode plugin added

--- JS Plugins/Dark-Light-Mode/index.js
+++ JS Plugins/Dark-Light-Mode/index.js
@@ -4,6 +4,25 @@ const sunPath = 'M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 2
 const darkMode = document.getElementById('darkMode')
 let toggle = false
 
+
 darkMode.addEventListener('click', () => {
-    
+    const timeline = anime.timeline({
+        duration: 740,
+        easing: 'easeOutExpo',
+    })
+    timeline.add({
+        targets: '.sun',
+        d: [{value: toggle ? sunPath : moonPath}]
+    })
+        .add({
+            targets: '#darkMode',
+            rotate: toggle ? 0 : 320,
+        }, '-= 440')
+        .add({
+            targets: 'section',
+            backgroundColor: toggle ? 'rgb(199, 199, 199)' : 'rgb(22, 22, 22)',
+            color: toggle ? 'rgb(22, 22, 22)' : 'rgb(199, 199, 199)'
+        }, '-=800')
+    toggle = !toggle
 })
+
