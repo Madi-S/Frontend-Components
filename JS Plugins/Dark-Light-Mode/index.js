53ada822df6a28e594e1960cbd5dbffdf7bffa40
Madi <72963376+Madi-S@users.noreply.github.com>
2021-05-23 16:02:47 +0600

    node modules

---
+++ JS Plugins/Dark-Light-Mode/index.js
@@ -0,0 +1,9 @@
+const moonPath = 'M14 28.2794C14 43.4672 27.5 55.7794 27.5 55.7794C12.3122 55.7794 0 43.4672 0 28.2794C0 13.0915 12.3122 0.779374 27.5 0.779374C27.5 0.779374 14 13.0915 14 28.2794Z'
+const sunPath = 'M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z'
+
+const darkMode = document.getElementById('darkMode')
+let toggle = false
+
+darkMode.addEventListener('click', () => {
+    
+})
