bc585bb7cd4dae80f23882e49a78bb94f2ec49eb
Madi <72963376+Madi-S@users.noreply.github.com>
2021-05-28 16:29:00 +0600

    drag and drop plugin implemented as class

--- JS Plugins/Drag-And-Drop/index.js
+++ JS Plugins/Drag-And-Drop/index.js
@@ -1,27 +1,83 @@
-const dropbase = document.querySelector('.dropbase')
-
-dropbase.addEventListener('dragover', event => {
-    event.preventDefault()
-    dropbase.classList.add('dropbase-hover')
-})
-
-dropbase.addEventListener('dragleave', event => {
-    dropbase.classList.remove('dropbase-hover')
-})
-
-dropbase.addEventListener('drop', event => {
-    event.preventDefault()
+`
+Before adding this script:
+1) Insert div with "dropbase" class
+2) Connect styles
+`
 
 
-    const files = event.dataTransfer.files
-    let valid = true
-    for (let file of files) {
-        console.log(file.type)
-        if (!file.type.includes('image')) {
-            valid = false
-        }
-    valid ? console.log('Files have valid extension') : console.warn('Only images are accepted')
-    valid ? dropbase.classList.add('dropbase-valid') : dropbase.classList.add('dropbase-invalid')
-    dropbase.textContent = valid ? 'Great, submit the image/images' : 'Wrong file format'
+const $dropbase = document.querySelector('.dropbase')
+class DragAndDrop {
+    constructor() {
+        this.destroyed = false
+        this.activate()
     }
-})
+
+    activate() {
+        if (!this.destroyed) {
+            $dropbase.addEventListener('dragleave', this._dragLeavehandler)
+            $dropbase.addEventListener('dragover', this._dragOverHandler)
+            $dropbase.addEventListener('drop', this._dropHandler)
+
+            $dropbase.textContent = 'Drag Files Here'
+        }
+    }
+
+    deactivate() {
+        if (!this.destroyed) {
+            $dropbase.removeEventListener('dragleave', this._dragLeavehandler)
+            $dropbase.removeEventListener('dragover', this._dragOverHandler)
+            $dropbase.removeEventListener('drop', this._dropHandler)
+
+            $dropbase.classList.remove('dropbase-hover')
+            $dropbase.classList.remove('dropbase-valid')
+            $dropbase.classList.remove('dropbase-invalid')
+
+            $dropbase.textContent = 'Deactivated'
+        }
+    }
+
+    destroy() {
+        $dropbase.parentNode.removeChild($dropbase)
+        this.destroyed = true
+    }
+
+    // *** Event handlers ***
+
+    _dragLeavehandler() {
+        $dropbase.classList.remove('dropbase-hover')
+    }
+
+    _dragOverHandler(event) {
+        event.preventDefault()
+        $dropbase.classList.add('dropbase-hover')
+    }
+
+    _dropHandler(event) {
+        console.log('in drop', this)
+        event.preventDefault()
+
+        const files = event.dataTransfer.files
+        const filesData = []
+        let valid = true
+        for (let file of files) {
+            console.log(file)
+            filesData.push(file)
+        }
+
+        if (valid) {
+            dropbase.classList.add('dropbase-valid')
+            dropbase.textContent = 'Great, submit image/images'
+        } else {
+            console.alert('Wrong file type')
+            dropbase.classList.add('dropbase-invalid')
+            dropbase.textContent = 'Wrong file type'
+        }
+
+        DragAndDrop._sendFilesToServer(filesData, 'Nothing')
+    }
+
+    // *** Send files bytes data via API ***
+    static _sendFilesToServer(data, user_id) {}
+
+}
+const d = new DragAndDrop
